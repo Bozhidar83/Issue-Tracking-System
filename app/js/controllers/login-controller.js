89a2ebd94047056b793cafaf8797ff5f30255e40
Bozhidar83 <b.chakurov@abv.bg>
2016-04-22 23:26:20 +0300

    fix unbreakable long words

--- app/js/controllers/login-controller.js
+++ app/js/controllers/login-controller.js
@@ -7,17 +7,19 @@
             '$location',
             'authService',
             'notifyService',
-            function LoginController($scope, $location, authService, notifyService) {
-                $scope.login = function(user, loginForm) {
-                    if (loginForm.$valid) {
-                        authService.login(user)
-                            .then(function() {
-                                notifyService.showInfo('Login successful');
-                                $location.path('#/');
-                            }, function(error) {
-                                notifyService.showError("Login failed", error);
-                            });
-                    }
+            'usSpinnerService',
+            function LoginController($scope, $location, authService, notifyService, usSpinnerService) {
+                $scope.login = function(user) {
+                    usSpinnerService.spin('spinner-1');
+                    authService.login(user)
+                        .then(function() {
+                            notifyService.showInfo('Login successful');
+                            $location.path('#/');
+                            usSpinnerService.stop('spinner-1');
+                        }, function(error) {
+                            notifyService.showError("Login failed", error);
+                            usSpinnerService.stop('spinner-1');
+                        });
                 };
             }
         ]);
